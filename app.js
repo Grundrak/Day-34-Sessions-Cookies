0908b53092d13902ba679400f03a123446fe629a
Grundrak <electrovilla66@gmail.com>
2023-09-29 23:18:13 +0100

    repush

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const session = require('express-session');
+const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
-// Server Variable Structure
+
+const users = [
+  {
+    username: 'alice',
+    password: 'hashed_password',
+    username: 'ahmed',
+    password: 'password',
+    username: 'nall',
+    password: 'toro',
+  },
+
+];
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use(express.json());
 app.set('view engine','ejs');
 app.set('views','./views');
 
-
-
-
-
-
 const registrationRoutes = require('./routes/registration');
 app.use('/register', registrationRoutes);
-
-
+const loginRoutes = require('./routes/login')
+app.use('/login', loginRoutes);
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
   });
--- routes/registration.js
+++ routes/registration.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
     res.render('registration', req.query);
       });
 
-      const users = [
-        {
-          username: 'alice',
-          password: 'hashed_password',
-        },
-        // Add more user objects as needed
-      ];
-      router.post('/submit', (req, res) => {
+
+      router.post('/submit', async (req, res) => {
         const username = req.body.username;
         const password = req.body.password;
-      
-        // Process the registration data (e.g., save to a database)
-        // Redirect or render a success page
+        const hashedPass = await bcrypt.hash(password ,10)
+        users.push({username, password: hashedPass});
         res.render('registration-success', { username });
       });
 
